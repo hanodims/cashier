70a0e2e8e0dbc826b5cb1b21434502bfc274b7bc
hanodims <hanodims@hotmail.com>
2020-08-18 10:35:22 +0300

    new

--- cashier.py
+++ cashier.py
@@ -1,5 +1,23 @@
 def main():
-	# write code here
+	receipt = []
+	total = .0
+	item = input("Item (enter \"done\" when finished): ")
+	while item.lower() != "done":
+		choice = {}
+		price = input("Price: ")
+		quantity = input("quantity: ")
+		choice = {
+			'name' : item,
+			'price' : price,
+			'quantity' : quantity
+			}
+		receipt.append(choice)
+		item = input("Item (enter \"done\" when finished): ")
+	for i in receipt:
+		price = int(i['quantity']) * float(i['price'])
+		print('%s' % i['quantity'],i['name'],price)
+		total += price
+	print("Total Price: %fKD" % total)
 
 if __name__ == '__main__':
 	main()
